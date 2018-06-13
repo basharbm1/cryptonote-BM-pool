ce6e21109f49cb183be27fc93e122a0e23456510
basharbm <basharbm@gmail.com>
2018-06-13 03:45:20 +0300

    initial commit

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "http://127.0.0.1:8117";
+
+var poolHost = "pool.ex.ch";
+
+var irc = "irc.freenode.net/#ex";
+
+var email = "support@ex.ch";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.ex.com/block/{id}";
+
+var transactionExplorer = "http://explorer.ex.com/tx/{id}";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
